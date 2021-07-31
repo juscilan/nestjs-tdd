eca4db78115479e4c0a0e989e8ce556f852f9f11
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-07-30 23:02:08 -0300

    feat(): adicionar configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3307,
-    username: 'root',
-    password: 'root',
-    database: 'nestjs_tdd',
-    entities: [],
-    synchronize: true,
+  imports: [TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('HOST', 'localhost'),
+        port: Number(configService.get('PORT', 3307)),
+        username: configService.get('USERNAME', 'root'),
+        password: configService.get('PASSWORD', 'root'),
+        database: configService.get('DATABASE', 'nestjs_tdd'),
+        entities: [],
+        synchronize: true,   
+    })
   }),
   ],
   controllers: [],
