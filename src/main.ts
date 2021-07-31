d1abe78d5d0606d61e31183b7a30be1c3dc77408
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-07-30 22:21:49 -0300

    feat(): adicionar porta vinda dos env

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000;
+  const PORT = process.env.NEST_JS_PORT || 3000;
   await app.listen(PORT);
 }
 bootstrap();
