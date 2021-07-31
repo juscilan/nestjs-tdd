d569d75a4d5b548171e6ca15a7063d9d7ef1932b
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-07-30 22:47:49 -0300

    fix(orm): arrumar dependencias typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3307,
+    username: 'root',
+    password: 'root',
+    database: 'nestjs_tdd',
+    entities: [],
+    synchronize: true,
+  }),
+  ],
   controllers: [],
   providers: [],
 })
