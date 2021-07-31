ee0faa78fafd03bb83dd07d33f331aa887a3fcf1
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-07-30 23:44:52 -0300

    feat(): adicioanr todo service implementacoes methodos

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity) 
+    private readonly todoRepository: Repository<TodoEntity>) {}
+
+    async findAll(){
+      return await this.todoRepository.find();
+    }
+
+    async findOne(id: string){
+      return await this.todoRepository.findOne(id);
+    }
+
+    async create(data){
+      return await this.todoRepository.save(this.todoRepository.create(data));
+    }
+
+    async update(id: string, data){
+      const todo = await this.findOne(id);
+      this.todoRepository.merge(todo, data)
+      return await this.todoRepository.save(todo);
+    }
+
+    async delete(id: string){
+      const todo = await this.findOne(id);
+      return await this.todoRepository.softDelete(todo);
+    }
+}
