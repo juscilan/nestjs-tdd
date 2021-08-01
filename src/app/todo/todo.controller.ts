05edf51cf253324c0122c5b7789b96fdf3b8d011
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-08-01 14:23:20 -0300

    feat(): adiconar response no swagger

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPi
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateTodoDto } from '../dto/todo-create.dto';
 import { UpdateTodoDto } from '../dto/todo-upadate.dto';
+import { TodoEntity } from './entity/todo.entity';
 import { TodoService } from './todo.service';
 
 @Controller('api/v1/todos')
@@ -12,14 +13,14 @@ export class TodoController {
 
   @Get()
   @ApiOperation({summary: 'Listar todos os dados das tarefas'})
-  @ApiResponse({status: 200, description: 'Lista de tarefas'})
+  @ApiResponse({status: 200, description: 'Lista de tarefas', type: TodoEntity, isArray: true})
   async findAll(){
     return this.todoService.findAll();
   }
 
   @Get(':id')
   @ApiOperation({summary: 'Listar uma tarefa'})
-  @ApiResponse({status: 200, description: 'Lista uma tarefa'})
+  @ApiResponse({status: 200, description: 'Lista uma tarefa', type: TodoEntity})
   @ApiResponse({status: 400, description: 'Parametros invalidos'})
   async findOne(@Param('id', new ParseUUIDPipe()) id: string){
     const todo = await this.todoService.findOne(id);
@@ -30,14 +31,14 @@ export class TodoController {
 
   @Post()
   @ApiOperation({summary: 'Cria uma tarefa'})
-  @ApiResponse({status: 201, description: 'Tarefa criada com sucesso'})
+  @ApiResponse({status: 201, description: 'Tarefa criada com sucesso', type: TodoEntity})
   async create(@Body() body: CreateTodoDto){
     return await this.todoService.create(body);
   }
 
   @Put(':id')
   @ApiOperation({summary: 'Atualiza uma tarefa'})
-  @ApiResponse({status: 200, description: 'Tarefa atualizada'})
+  @ApiResponse({status: 200, description: 'Tarefa atualizada', type: TodoEntity})
   @ApiResponse({status: 400, description: 'Parametros invalidos'})
   async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: UpdateTodoDto){  
     const todo = await this.todoService.findOne(id);
