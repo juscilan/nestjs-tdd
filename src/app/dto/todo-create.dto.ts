345eb4389b4b1a00e56d3696d513a2915dacfd45
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-08-01 14:02:00 -0300

    feat(): adicionar detalhes swagger

--- src/app/dto/todo-create.dto.ts
+++ src/app/dto/todo-create.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsIn, IsNotEmpty } from "class-validator";
 
 export class CreateTodoDto {
   @IsNotEmpty()
+  @ApiProperty()
   task: string;
 
   @IsNotEmpty()
   @IsIn([0,1])
+  @ApiProperty()
+  // @ApiPropertyOptional()
   isDone: number;
 }
--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,19 +1,26 @@
 import { NotFoundException } from '@nestjs/common';
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateTodoDto } from '../dto/todo-create.dto';
 import { UpdateTodoDto } from '../dto/todo-upadate.dto';
 import { TodoService } from './todo.service';
 
 @Controller('api/v1/todos')
+@ApiTags('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService){}
 
   @Get()
+  @ApiOperation({summary: 'Listar todos os dados das tarefas'})
+  @ApiResponse({status: 200, description: 'Lista de tarefas'})
   async findAll(){
     return this.todoService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary: 'Listar uma tarefa'})
+  @ApiResponse({status: 200, description: 'Lista uma tarefa'})
+  @ApiResponse({status: 400, description: 'Parametros invalidos'})
   async findOne(@Param('id', new ParseUUIDPipe()) id: string){
     const todo = await this.todoService.findOne(id);
     if(!todo)
@@ -22,11 +29,16 @@ export class TodoController {
   }
 
   @Post()
+  @ApiOperation({summary: 'Cria uma tarefa'})
+  @ApiResponse({status: 201, description: 'Tarefa criada com sucesso'})
   async create(@Body() body: CreateTodoDto){
     return await this.todoService.create(body);
   }
 
   @Put(':id')
+  @ApiOperation({summary: 'Atualiza uma tarefa'})
+  @ApiResponse({status: 200, description: 'Tarefa atualizada'})
+  @ApiResponse({status: 400, description: 'Parametros invalidos'})
   async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: UpdateTodoDto){  
     const todo = await this.todoService.findOne(id);
     if(!todo)
@@ -35,6 +47,9 @@ export class TodoController {
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'Eclui uma tarefa'})
+  @ApiResponse({status: 204, description: 'Tarefa exluida'})
+  @ApiResponse({status: 400, description: 'Parametros invalidos'})
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('id', new ParseUUIDPipe()) id: string){
     return await this.todoService.delete(id);
