d5f0bb789bb7c1d14b226c30f7e1d653415a9a6a
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-08-01 13:44:51 -0300

    feat(): adicionar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo App api')
+    .setDescription('')
+    .setVersion('0.0.1')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   app.useGlobalPipes(new ValidationPipe())
   const PORT = process.env.NEST_JS_PORT || 3000;
   await app.listen(PORT);
