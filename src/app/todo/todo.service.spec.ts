7f6848d712321c996d2fae96acefadbd78a6e535
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-08-01 17:29:42 -0300

    feat(): adicionar testes unitarios na camada service

--- src/app/todo/todo.service.spec.ts
+++ src/app/todo/todo.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindRelationsNotFoundError, Repository } from 'typeorm';
 import { TodoEntity } from './entity/todo.entity';
 import { TodoService } from './todo.service';
 
+const todoEntityList: TodoEntity[] = [
+  new TodoEntity({id: '1', task: 'task-one'}),
+  new TodoEntity({id: '2', task: 'task-two'}),
+  new TodoEntity({id: '3', task: 'task-three'})
+]
+
 describe('TodoService', () => {
-  let service: TodoService;
+  let sut: TodoService;
+  let todoRepository: Repository<TodoEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,16 +21,36 @@ describe('TodoService', () => {
       {
         provide: getRepositoryToken(TodoEntity),
         useValue: {
-          
+          find: jest.fn().mockResolvedValue(todoEntityList),
+          findOne: jest.fn(),
+          save: jest.fn(),
+          create: jest.fn(),
+          merge: jest.fn(),
+          delete: jest.fn(),
         }
       }
     ],
     }).compile();
 
-    service = module.get<TodoService>(TodoService);
+    sut = module.get<TodoService>(TodoService);
+    todoRepository = module.get<Repository<TodoEntity>>(getRepositoryToken(TodoEntity));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(sut).toBeDefined();
+    expect(todoRepository).toBeDefined();
   });
+
+  describe('findAll', () => {
+    test('should return a todo list of TodoEntity', async () => {
+      const result = await sut.findAll();
+      expect(result).toEqual(todoEntityList);
+      expect(todoRepository.find).toHaveBeenCalledTimes(1);
+    });
+    test('should throw an exception', () => {
+      jest.spyOn(todoRepository, 'find').mockRejectedValueOnce(new Error());
+      expect(sut.findAll()).rejects.toThrowError();
+    });  
+
+  })
 });
