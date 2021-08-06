a5b82252df62010fabf5134b0529207b945c68bc
Juscilan Moreto <juscilan.moreto@savecash.com.br>
2021-08-06 18:43:24 -0300

    test(): mocked unit tests for todo controller

--- src/app/todo/todo.controller.spec.ts
+++ src/app/todo/todo.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let controller: TodoController;
@@ -7,6 +8,16 @@ describe('TodoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
+      providers: [{
+        provide: TodoService,
+        useValue: {
+          findAll: jest.fn(),
+          findOne: jest.fn(),
+          create: jest.fn(),
+          update: jest.fn(),
+          delete: jest.fn()
+        }
+      }]
     }).compile();
 
     controller = module.get<TodoController>(TodoController);
